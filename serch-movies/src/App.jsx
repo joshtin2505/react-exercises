3a0d9bb7550faab03708e03b746f0d2d1f132b18
joshtin2505 <justincastro2505@gmail.com>
2023-09-15 17:15:59 -0500

    serch movies

--- serch-movies/src/App.jsx
+++ serch-movies/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-
+import useSerch from './hooks/useSerch'
 function App() {
   const {movies} = useMovies()
+  const [serch, setSerch, err] = useSerch()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const fields = Object.fromEntries( new FormData(event.target))
+    const fields = serch
 
     /* 
     para obtener un solo value
@@ -15,20 +16,33 @@ function App() {
       const fields = new FormData(event.target)
       const query = fields.get('query')
     */
+    /* 
+    para obtener un solo value
+    Esta es otra manera de obtener el value
+      const fields = Object.fromEntries( new FormData(event.target))
+    */
 
     //Tambien puedes obtener el value de esta manera ("event.target.query.value") aqui debes llenar el nombre del input en este caso "query" y luego poner la propiedad del input que quieres en este caso un "value"
     console.log(fields)
   }
-
+  const handleChange = (e) =>{
+    
+    setSerch(e.target.value)
+  }
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input name='query' placeholder="God's not Dead, I can Only Imagen...." />
-          <input name='toro' placeholder="God's not Dead, I can Only Imagen...." />
+          <input value={serch} style={ err && {border: '2px solid red'}} onChange={handleChange} name='query' placeholder="God's not Dead, I can Only Imagen...." />
           <button type="submit">Search</button>
         </form>
+        {
+          err && <p style={{
+            color: 'red'
+            }}>{err}</p>
+        }
+        
       </header>
 
       <main>
